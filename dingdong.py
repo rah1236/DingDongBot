810d01ad876925da90fda0c6131541415a8c3f73
rah1236 <rehmatullah4@gmail.com>
2018-05-26 23:46:42 +0000

    Cut out some fat

--- dingdong.py
+++ dingdong.py
@@ -1,5 +1,3 @@
-import re
-import json
 import time #for delay (in seconds)
 import pygame #used for mixer to play doorbell sound
 from slackclient import SlackClient #Used for slack client
@@ -13,8 +11,7 @@ pygame.mixer.music.load("Ding-dong-chime.mp3") #load doorbell sound
 
 while True: #loop forever
 	for message in slack_client.rtm_read():
-		if 'text' in message and message['text'].startswith("!"):
-                	print "Message received: %s" % json.dumps(message, indent=2)
-			if re.match(r'.*(dingdong).*', message['text'], re.IGNORECASE):
-                  		pygame.mixer.music.play()
+		if 'text' in message and message['text'].startswith("!dingdong"):
+                  	pygame.mixer.music.play()
+			print "Doorbell Rang!"
 	time.sleep(1) #delay for 1 second
